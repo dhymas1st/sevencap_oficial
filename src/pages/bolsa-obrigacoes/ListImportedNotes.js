677ad3da2eb38db40a8e638a2a392a4d9d3bcc54
lCherS <lucascherez.cl@gmail.com>
2022-09-28 14:23:28 -0300

    ajustando tabela

--- src/pages/bolsa-obrigacoes/ListImportedNotes.js
+++ src/pages/bolsa-obrigacoes/ListImportedNotes.js
@@ -1,8 +1,14 @@
-import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
 import { useEffect, useState } from 'react';
+import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+
+import SnackbarAlert from '../../components/SnackbarAlert';
+
 import api from '../../services/api';
 const ListImportedNotes = () => {
     const [notas, SetNotas] = useState([]);
+    const [snackbar, setSnackBar] = useState([]);
 
     useEffect(() => {
         const dados = JSON.parse(localStorage.getItem('dados'));
@@ -12,15 +18,19 @@ const ListImportedNotes = () => {
             SetNotas(res.data);
         });
     }, []);
+
+    function handleClick() {
+        setSnackBar([true, 'warning', 'botão de deletar ainda desativado']);
+    }
     return (
         <TableContainer component={Paper} sx={{ maxHeight: '300', mt: 4 }}>
             <Table aria-label="semple table" stickyHeader>
                 <TableHead>
                     <TableRow>
                         <TableCell>Data Upload</TableCell>
-                        <TableCell align="right">Nome do Arquivo</TableCell>
-                        <TableCell align="right">Status</TableCell>
-                        <TableCell align="right">Editar</TableCell>
+                        <TableCell>Nome do Arquivo</TableCell>
+                        <TableCell>Status</TableCell>
+                        <TableCell>Editar</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -28,12 +38,17 @@ const ListImportedNotes = () => {
                         <TableRow key={i} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
                             <TableCell>{row.data_upload.substr(0, 10).split('-').reverse().join('/')}</TableCell>
                             <TableCell>{row.nome_original_arquivo}</TableCell>
-                            <TableCell>{row.status}</TableCell>
-                            <TableCell>icon</TableCell>
+                            <TableCell>{row.status === 'S' ? 'Nota ainda em processamento' : 'Nota lida com sucesso'}</TableCell>
+                            <TableCell>
+                                <IconButton onClick={handleClick}>
+                                    <DeleteIcon />
+                                </IconButton>
+                            </TableCell>
                         </TableRow>
                     ))}
                 </TableBody>
             </Table>
+            {snackbar[0] === true ? <SnackbarAlert tipo={snackbar[1]} title={snackbar[2]} /> : ''}
         </TableContainer>
     );
 };
